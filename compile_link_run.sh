fd848f4a1f515e2a0c4f4a6b33a0fcbbc87c38d1
Binkhorst, Gérard <gerard.binkhorst@kpn.com>
2021-09-16 17:30:10 +0200

    renamed compile_run script

--- compile_link_run.sh
+++ compile_link_run.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+
+# remove any previous results to avoid confusion
+rm ./bin//3dsimulations.a
+
 # compile
 gcc -I./include -c ./src/lapack_shell.c -o ./bin/lapack_shell.o 
 gcc -I./include -c ./src/3dsimulations.c -o ./bin/3dsimulations.o 
