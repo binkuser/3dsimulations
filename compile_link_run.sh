0ff10b96f978737c81d8b3be473d7193548e6e2c
Gérard Binkhorst <gerard@binkhorst.org>
2022-06-06 22:35:31 +0200

    update

--- compile_link_run.sh
+++ compile_link_run.sh
@@ -9,7 +9,7 @@ gcc -I./include -c ./src/3dsimulations.c -o ./bin/3dsimulations.o
 gcc -I./include -c ./src/bars.c -o ./bin/bars.o 
 
 # link
-gcc -o ./bin/3dsimulations.a ./bin/lapack_shell.o ./bin/3dsimulations.o  ./bin/bars.o -lm -lblas -llapack -llapacke 
+gcc -L ./lib -o ./bin/3dsimulations.a ./bin/lapack_shell.o ./bin/3dsimulations.o  ./bin/bars.o -lm -lblas -llapack -llapacke
 
 # clean
 rm ./bin/lapack_shell.o 
